15b10373cb68c5e74d5ffcc1241c9cd878f0ddce
Alex <oleksandr.bodnarchuk@co3.eu>
2024-06-01 16:28:51 +0200

    Home page finished.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { HomeCards } from "./components/HomeCards";
+import { JobListings } from "./components/JobListings";
+import { ViewAllJobs } from "./components/ViewAllJobs";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
 }
 
-export default App
+export default App;
+
