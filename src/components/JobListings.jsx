7b390ffff4b74f8b1ea43f0ed42783df8ce87686
Alex <oleksandr.bodnarchuk@co3.eu>
2024-06-08 16:41:55 +0200

    changed connection to my Spring backend

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -8,7 +8,7 @@ export const JobListings = ({ isHome = false }) => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs"
+      const apiUrl = isHome ? "/api/jobs?limit=3" : "/api/jobs"
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
