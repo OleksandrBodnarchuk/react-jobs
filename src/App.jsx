a01980512159582f47c0ad7215be9eca4c547ee5
Alex <oleksandr.bodnarchuk@co3.eu>
2024-06-20 20:16:10 +0200

    Edit job page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import { JobsPage } from "./pages/JobsPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 function App() {
   // Add new job
@@ -40,6 +41,21 @@ function App() {
     return;
   };
 
+  // Edit job
+  const updateJob = async (job) => {
+    console.log(job);
+    const response = await fetch(`/api/jobs/update/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(job),
+    });
+    const data = await response.json();
+    console.log(data);
+    return;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
@@ -55,6 +71,11 @@ function App() {
           loader={jobLoader}
         />
         <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route
+          path="/jobs/edit/:id"
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -86,7 +86,7 @@ const JobPage = ({ deleteJob }) => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to="/jobs/add"
+                  to={`/jobs/edit/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
